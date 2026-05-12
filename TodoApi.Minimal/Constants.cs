646e6a41e0a35d86b51442867d246b3b7b2c323b
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-12 13:39:10 -0400

    feat(Constants): add constants for Swagger UI endpoint grouping

---
+++ TodoApi.Minimal/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApi.Minimal;
+
+/// <summary>
+/// Contains constant values used throughout the application, such as tags for grouping endpoints in the Swagger UI.
+/// </summary>
+public static class Constants
+{
+    /// <summary>
+    /// Constant used to group all endpoints under a single tag in the Swagger UI
+    /// </summary>
+    public const string TodoItemsTag = "TodoItems";
+}
