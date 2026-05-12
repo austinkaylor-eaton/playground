dceee201210e32079634195f82ade947f99a3b1c
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-12 12:07:15 -0400

    feat(database): add TodoDb context for managing TodoItem entities

---
+++ TodoApi.Minimal/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Minimal;
+
+using Microsoft.EntityFrameworkCore;
+
+public class TodoDb(DbContextOptions<TodoDb> options) : DbContext(options)
+{
+    /// <summary>
+    /// Gets the DbSet of to-do items, allowing for querying and saving instances of the <see cref="TodoItem"/> entity.
+    /// </summary>
+    public DbSet<TodoItem> Todos => Set<TodoItem>();
+}
