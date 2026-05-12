d1bb7229ea12fe1ef59c201aa718c2f549e8bdcb
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-12 13:19:13 -0400

    docs(TodoDb): add XML documentation for database context class

--- TodoApi.Minimal/TodoDb.cs
+++ TodoApi.Minimal/TodoDb.cs
@@ -2,6 +2,10 @@
 
 using Microsoft.EntityFrameworkCore;
 
+/// <summary>
+/// Represents the Entity Framework Core database context for the to-do application, providing access to the to-do items stored in the database.
+/// </summary>
+/// <param name="options">The options used to configure the database context.</param>
 public class TodoDb(DbContextOptions<TodoDb> options) : DbContext(options)
 {
     /// <summary>
