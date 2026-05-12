9e98031b911a59843faec098fc06f53c43a15801
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-12 12:14:15 -0400

    feat(api): implement CRUD operations for Todo items

--- TodoApi.Minimal/Program.cs
+++ TodoApi.Minimal/Program.cs
@@ -1,38 +1,58 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Minimal;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+#pragma warning disable CA2007
+app.MapGet("/todoitems", async (TodoDb db) =>
+    await db.Todos.ToListAsync());
+
+app.MapGet("/todoitems/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
+
+app.MapPost("/todoitems", async (TodoItem todo, TodoDb db) =>
 {
-    app.MapOpenApi();
-}
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
 
-app.UseHttpsRedirection();
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
 
-var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+app.MapPut("/todoitems/{id}", async (int id, TodoItem inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
 
-app.MapGet("/weatherforecast", () =>
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is not { } todo)
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+        return Results.NotFound();
+    }
 
-app.Run();
+    db.Todos.Remove(todo);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+});
+#pragma warning restore CA2007
+
+await app.RunAsync().ConfigureAwait(false);
