2fcd9069ea505d49d8e435a538d9d41920b241ce
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-12 15:08:41 -0400

    feat(constants): add constant for TodoItem ID URL path

--- TodoApi.Minimal/Constants.cs
+++ TodoApi.Minimal/Constants.cs
@@ -11,6 +11,18 @@
             /// Constant used to group all endpoints under a single tag in the Swagger UI
             /// </summary>
             public const string Tag = "TodoItems";
+
+            /// <summary>
+            /// Constant used to define the URL path for any TodoItem endpoints that retrieves a specific TodoItem by its ID
+            /// </summary>
+            /// <remarks>
+            /// Used by the following endpoints: <br/>
+            /// - GET /{id:int} <br/>
+            /// - PUT /{id:int} <br/>
+            /// - PATCH /{id:int} <br/>
+            /// - DELETE /{id:int} <br/>
+            /// </remarks>
+            public const string IdUrlPath = "{id:int}";
         }
     }
 }
