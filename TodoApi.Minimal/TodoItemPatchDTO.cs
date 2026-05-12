572588c5bf754d75e282879e87bef8c9fcce6bdb
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-12 13:49:38 -0400

    feat(api): add TodoItemPatchDTO for PATCH requests

---
+++ TodoApi.Minimal/TodoItemPatchDTO.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+
+namespace TodoApi.Minimal;
+
+/// <summary>
+/// A <see href="https://en.wikipedia.org/wiki/Data_transfer_object">data transfer object</see>
+/// representing a <see href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Methods/PATCH">PATCH</see> request for a <see cref="TodoItem"/>.
+/// </summary>
+/// <remarks>
+/// This is used to decouple the internal database representation of a TodoItem from the external representation sent to clients,
+/// allowing for more flexibility in changing the database schema without affecting the API contract with clients. <br/>
+/// </remarks>
+[PublicAPI]
+public class TodoItemPatchDTO
+{
+    /// <inheritdoc cref="TodoItem.Name"/>
+    public string? Name { get; set; }
+    /// <inheritdoc cref="TodoItem.IsComplete"/>
+    public bool? IsComplete { get; set; }
+}
