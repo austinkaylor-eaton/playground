02a9faad8ef4bb5ab8f54db9fa9bda43c2187e25
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-12 13:16:03 -0400

    feat(api): add Swagger middleware for API documentation

--- TodoApi.Minimal/Program.cs
+++ TodoApi.Minimal/Program.cs
@@ -4,6 +4,31 @@ using TodoApi.Minimal;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+#region Swagger Middleware
+
+/*
+ * Enables the API Explorer, which is a service that provides metadata about the HTTP API.
+ * The API Explorer is used by Swagger to generate the Swagger document.
+ */
+builder.Services.AddEndpointsApiExplorer();
+/*
+ * Adds the Swagger OpenAPI document generator to the application services and
+ * configures it to provide more information about the API, such as its title and version
+ *
+ * See https://learn.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-nswag?view=aspnetcore-8.0&viewFallbackFrom=aspnetcore-10.0&tabs=visual-studio#customize-api-documentation
+ * for more information
+ */
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "TodoAPI";
+    config.Title = "TodoAPI v1";
+    config.Version = "v1";
+});
+
+#endregion
+
+
 var app = builder.Build();
 
 #pragma warning disable CA2007
@@ -13,7 +38,7 @@ app.MapGet("/todoitems", async (TodoDb db) =>
 app.MapGet("/todoitems/complete", async (TodoDb db) =>
     await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+app.MapGet("/todoitems/{id:int}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
         is { } todo
         ? Results.Ok(todo)
@@ -27,7 +52,7 @@ app.MapPost("/todoitems", async (TodoItem todo, TodoDb db) =>
     return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
-app.MapPut("/todoitems/{id}", async (int id, TodoItem inputTodo, TodoDb db) =>
+app.MapPut("/todoitems/{id:int}", async (int id, TodoItem inputTodo, TodoDb db) =>
 {
     var todo = await db.Todos.FindAsync(id);
 
@@ -41,7 +66,7 @@ app.MapPut("/todoitems/{id}", async (int id, TodoItem inputTodo, TodoDb db) =>
     return Results.NoContent();
 });
 
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+app.MapDelete("/todoitems/{id:int}", async (int id, TodoDb db) =>
 {
     if (await db.Todos.FindAsync(id) is not { } todo)
     {
