0beda3dc7eed1d53ac3f97cc9c956a4cc74a12bd
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-13 09:18:07 -0400

    feat(models): add Currency enum for book pricing

---
+++ BookStoreApi/Models/Currency.cs
@@ -0,0 +1,21 @@
+﻿namespace BookStoreApi.Models;
+
+/// <summary>
+/// Represents the currency used for pricing books in the bookstore.
+/// </summary>
+/// <remarks>
+/// Directly used by the <see cref="Money"/> struct
+/// </remarks>
+public enum Currency
+{
+    USD, // US Dollar
+    EUR, // Euro
+    GBP, // British Pound
+    JPY, // Japanese Yen
+    AUD, // Australian Dollar
+    CAD, // Canadian Dollar
+    CHF, // Swiss Franc
+    CNY, // Chinese Yuan
+    SEK, // Swedish Krona
+    NZD  // New Zealand Dollar
+}
