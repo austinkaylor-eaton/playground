8b580d5dacbaa5aa8ebf0e6b6944df0c02a98c68
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-13 08:25:31 -0400

    feat(models): add BookCategory enum for categorizing books

---
+++ BookStoreApi/Models/BookCategory.cs
@@ -0,0 +1,20 @@
+﻿namespace BookStoreApi.Models;
+
+/// <summary>
+/// Represents the category of a book in the bookstore.
+/// </summary>
+public enum BookCategory
+{
+    Fiction,
+    NonFiction,
+    Science,
+    History,
+    Biography,
+    Children,
+    Fantasy,
+    Mystery,
+    Romance,
+    Horror,
+    Computers,
+    Uncategorized
+}
