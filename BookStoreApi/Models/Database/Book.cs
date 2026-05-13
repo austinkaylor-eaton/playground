2c81e7713f1c3d3700fdb87bc7909bdff89bcb75
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-13 08:15:58 -0400

    feat(models): add Book model for MongoDB integration

---
+++ BookStoreApi/Models/Database/Book.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BookStoreApi.Models.Database;
+
+/// <summary>
+/// Represents a book in the bookstore.
+/// </summary>
+/// <remarks>
+/// This class is used to map the book data stored in the MongoDB database.
+/// </remarks>
+public class Book
+{
+    /// <summary>
+    /// A unique identifier for the book.
+    /// </summary>
+    /// <remarks>
+    /// This is the primary key in the MongoDB collection.
+    /// </remarks>
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    /// <summary>
+    /// The name of the book.
+    /// </summary>
+    [BsonElement("Name")]
+    public string BookName { get; set; } = null!;
+
+    /// <summary>
+    /// The price of the book.
+    /// </summary>
+    [BsonElement("Price")]
+    public decimal Price { get; set; }
+
+    /// <summary>
+    /// The category of the book.
+    /// </summary>
+    [BsonElement("Category")]
+    public string Category { get; set; } = null!;
+
+    /// <summary>
+    /// The author of the book.
+    /// </summary>
+    [BsonElement("Author")]
+    public string Author { get; set; } = null!;
+}
