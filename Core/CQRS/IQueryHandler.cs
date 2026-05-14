3b0116682586858b4f731cd013757147ec87369e
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-14 08:46:00 -0400

    feat(cqrs): add ICommandHandler and IQueryHandler interfaces

---
+++ Core/CQRS/IQueryHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.CQRS;
+
+public interface IQueryHandler<in TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+{
+    Task<TResponse> Handle(TQuery query, CancellationToken cancellationToken);
+}
