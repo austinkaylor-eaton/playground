4c67df25a25858440780573da9ef0183c3dffbf7
Austin Kaylor <austinjkaylor@eaton.com>
2026-05-14 09:56:15 -0400

    feat(logging): add logging methods for command and query handling

--- Core/CQRS/Decorators/Log.cs
+++ Core/CQRS/Decorators/Log.cs
@@ -8,22 +8,57 @@ namespace Core.CQRS.Decorators;
 /// </summary>
 internal static partial class Log
 {
+    /// <summary>
+    /// Logs that a command is about to be handled.
+    /// </summary>
+    /// <param name="logger">The logger instance.</param>
+    /// <param name="commandName">The name of the command type.</param>
     [LoggerMessage(Level = LogLevel.Information, Message = "Handling command {CommandName}")]
     internal static partial void HandlingCommand(ILogger logger, string commandName);
 
+    /// <summary>
+    /// Logs that a command was successfully handled, including elapsed time.
+    /// </summary>
+    /// <param name="logger">The logger instance.</param>
+    /// <param name="commandName">The name of the command type.</param>
+    /// <param name="elapsedMs">The elapsed time in milliseconds.</param>
     [LoggerMessage(Level = LogLevel.Information, Message = "Handled command {CommandName} in {ElapsedMs}ms")]
     internal static partial void HandledCommand(ILogger logger, string commandName, double elapsedMs);
 
+    /// <summary>
+    /// Logs that a command failed with an exception, including elapsed time.
+    /// </summary>
+    /// <param name="logger">The logger instance.</param>
+    /// <param name="ex">The exception that caused the failure.</param>
+    /// <param name="commandName">The name of the command type.</param>
+    /// <param name="elapsedMs">The elapsed time in milliseconds before the failure.</param>
     [LoggerMessage(Level = LogLevel.Error, Message = "Command {CommandName} failed after {ElapsedMs}ms")]
     internal static partial void CommandFailed(ILogger logger, Exception ex, string commandName, double elapsedMs);
 
+    /// <summary>
+    /// Logs that a query is about to be handled.
+    /// </summary>
+    /// <param name="logger">The logger instance.</param>
+    /// <param name="queryName">The name of the query type.</param>
     [LoggerMessage(Level = LogLevel.Information, Message = "Handling query {QueryName}")]
     internal static partial void HandlingQuery(ILogger logger, string queryName);
 
+    /// <summary>
+    /// Logs that a query was successfully handled, including elapsed time.
+    /// </summary>
+    /// <param name="logger">The logger instance.</param>
+    /// <param name="queryName">The name of the query type.</param>
+    /// <param name="elapsedMs">The elapsed time in milliseconds.</param>
     [LoggerMessage(Level = LogLevel.Information, Message = "Handled query {QueryName} in {ElapsedMs}ms")]
     internal static partial void HandledQuery(ILogger logger, string queryName, double elapsedMs);
 
+    /// <summary>
+    /// Logs that a query failed with an exception, including elapsed time.
+    /// </summary>
+    /// <param name="logger">The logger instance.</param>
+    /// <param name="ex">The exception that caused the failure.</param>
+    /// <param name="queryName">The name of the query type.</param>
+    /// <param name="elapsedMs">The elapsed time in milliseconds before the failure.</param>
     [LoggerMessage(Level = LogLevel.Error, Message = "Query {QueryName} failed after {ElapsedMs}ms")]
     internal static partial void QueryFailed(ILogger logger, Exception ex, string queryName, double elapsedMs);
 }
-
